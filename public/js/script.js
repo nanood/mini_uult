1b843be543a42923c24527413cac353a4e7d6178
nanood <48179031+nanood@users.noreply.github.com>
2025-12-17 15:47:53 +0800

    Add files via upload

---
+++ public/js/script.js
@@ -0,0 +1,153 @@
+const answers_no = {
+    english: [
+        "Үгүй",
+        "Итгэлтэй байна уу?",
+        "Үнэнээсээ юу??",
+        "Үнэхээрийн үгүй гэж үү???",
+        "Ахин бодох уу?",
+        "Ахиад нэг бодох уу?",
+        "Why ийм so cold?",
+        "Бид ярилцаж болох юм?",
+        "Би ахин асуухгүй шүү!",
+        "Энэ үйлдэл чинь намайг өвтгөж байна!",
+        "Yo are so mean!",
+        "Надад бүү ингэж хандаач?",
+        "Ахин нэг боломж олгоё!",
+        "Би чин сэтгэлээсээ гуйж байна!",
+        "За за эхнээс нь эхэлье.."
+    ],
+    french: [
+        "Non",
+        "Tu es sûr ?",
+        "Tu es vraiment sûr ??",
+        "Tu es vraiment vraiment sûr ???",
+        "Réfléchis encore?",
+        "Tu ne crois pas aux deuxièmes chances ?",
+        "Pourquoi tu es si froid?",
+        "Peut-être, on peut en parler ?",
+        "Je ne vais pas demander encore une fois!",
+        "D'accord, maintenant ca me fait mal!",
+        "Tu es juste méchant!",
+        "Pourquoi tu me fais ça?",
+        "Donnez-moi une chance plz!",
+        "Je te supplie d'arrêter!",
+        "D'accord, recommençons.."
+    ],
+    thai: [
+        "ไม่อ่ะ",
+        "แน่ใจจริงๆหรอคะ?",
+        "แน่ใจจริงๆ จริงๆนะคะ?",
+        "อย่าบอกนะว่านี่แน่ใจสุดๆแล้วจริงๆ ?",
+        "ลองคิดดูอีกทีหน่อยสิคะ..",
+        "ขอโอกาศที่สองทีค่ะ..",
+        "อย่าเย็นชาสิคะ กระซิกๆ",
+        "ขอร้องนะคะ",
+        "น้าาาๆๆๆๆๆ",
+        "เราจะร้องไห้เอานะ กระซิกๆ",
+        "จะเอางี้ๆจริงหรอคะ",
+        "ฮือออออ",
+        "ขอโอกาศครั้งที่สองที่ค่ะ!",
+        "ขอร้องละค่าาา",
+        "โอเคค่ะ.. งั้นเริ่มใหม่ !"
+    ]
+};
+
+answers_yes = {
+    "english": "Yes",
+    "french": "Oui",
+    "Thailand": "เย่ คืนดีกันแล้วน้า"
+}
+
+let language = "english"; // Default language is English
+const no_button = document.getElementById('no-button');
+const yes_button = document.getElementById('yes-button');
+let i = 1;
+let size = 50;
+let clicks = 0;
+
+no_button.addEventListener('click', () => {
+    // Change banner source
+    let banner = document.getElementById('banner');
+    if (clicks === 0) {
+        banner.src = "public/images/no.gif";
+        refreshBanner();
+    }
+    clicks++;
+    // increase button height and width gradually to 250px
+    const sizes = [40, 50, 30, 35, 45]
+    const random = Math.floor(Math.random() * sizes.length);
+    size += sizes[random]
+    yes_button.style.height = `${size}px`;
+    yes_button.style.width = `${size}px`;
+    let total = answers_no[language].length;
+    // change button text
+    if (i < total - 1) {
+        no_button.innerHTML = answers_no[language][i];
+        i++;
+    } else if (i === total - 1) {
+        alert(answers_no[language][i]);
+        i = 1;
+        no_button.innerHTML = answers_no[language][0];
+        yes_button.innerHTML = answers_yes[language];
+        yes_button.style.height = "50px";
+        yes_button.style.width = "50px";
+        size = 50;
+    }
+});
+
+yes_button.addEventListener('click', () => {
+    // change banner gif path
+    let banner = document.getElementById('banner');
+    banner.src = "public/images/yes.gif";
+    refreshBanner();
+    // hide buttons div
+    let buttons = document.getElementsByClassName('buttons')[0];
+    buttons.style.display = "none";
+    // show message div
+    let message = document.getElementsByClassName('message')[0];
+    message.style.display = "block";
+});
+
+function refreshBanner() {
+    // Reload banner gif to force load  
+    let banner = document.getElementById('banner');
+    let src = banner.src;
+    banner.src = '';
+    banner.src = src;
+}
+
+function changeLanguage() {
+    const selectElement = document.getElementById("language-select");
+    const selectedLanguage = selectElement.value;
+    language = selectedLanguage;
+
+    // Update question heading
+    const questionHeading = document.getElementById("question-heading");
+    if (language === "french") {
+        questionHeading.textContent = "Tu veux être mon valentin?";
+    } else if (language === "thai") {
+        questionHeading.textContent = "คืนดีกับเราได้อ่ะป่าว?";
+    } else {
+        questionHeading.textContent = "Will you be my valentine?";
+    }
+
+    // Reset yes button text
+    yes_button.innerHTML = answers_yes[language];
+
+    // Reset button text to first in the new language
+    if (clicks === 0) {
+        no_button.innerHTML = answers_no[language][0];
+    } else {
+        no_button.innerHTML = answers_no[language][clicks];
+    }
+
+    // Update success message
+    const successMessage = document.getElementById("success-message");
+    if (language === "french") {
+        successMessage.textContent = "Yepppie, à bientôt :3";
+    } else if (language === "thai") {
+        successMessage.textContent = "ฮูเร่ คืนดีกันแล้วน้า :3";
+    } else {
+        successMessage.textContent = "Yepppie, see you sooonnn :3";
+    }
+}
